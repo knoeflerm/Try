47a84592fe1d43eb04e083ff1a93552abd7491bc
Markus Knoefler <knoeflerm@gmail.com>
2012-08-03 18:16:01 +0200

    fixed broken unit test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "RoR Tutorial Sample App | Home")
     end
   end
 
@@ -26,7 +26,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "RoR Tutorial Sample App | Help")
     end
   end
 
@@ -40,7 +40,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "RoR Tutorial Sample App | About Us")
     end
   end
 end
