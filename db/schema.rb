21b0438c329e7373d8b94c86fd4affa02b3db9f9
Markus Knoefler <knoeflerm@gmail.com>
2012-10-05 13:27:24 +0200

    Finish uer edit, update, index and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121004095200) do
+ActiveRecord::Schema.define(:version => 20121005075122) do
 
   create_table "offers", :force => true do |t|
     t.string   "title"
@@ -21,21 +21,14 @@ ActiveRecord::Schema.define(:version => 20121004095200) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "tests", :force => true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
