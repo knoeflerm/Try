e1c60b412a2b8ff62df7712b0b638ae8d4dd12a2
Markus Knoefler <knoeflerm@gmail.com>
2012-10-10 11:35:50 +0200

    Put test code in describe general

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -4,42 +4,44 @@ describe "Micropost pages" do
 
   subject { page }
 
-  let(:user) { FactoryGirl.create(:user) }
-  before { sign_in user }
-
-  describe "micropost creation" do
-    before { visit root_path }
-
-    describe "with invalid information" do
-
-      it "should not create a micropost" do
-        expect { click_button "Submit" }.to_not change(Micropost, :count)
-      end
-
-      describe "error messages" do
-        let(:error) { '1 error prohibited this micropost from being saved' }
-        before { click_button "Submit" }
-        it { should have_content(error) } 
-      end
-    end
-
-    describe "with valid information" do
-
-      before { fill_in 'micropost_content', with: "Lorem ipsum" }
-      it "should create a micropost" do
-        expect { click_button "Submit" }.to change(Micropost, :count).by(1)
-      end
-    end
-  end
+  describe "general" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user }
   
-  describe "micropost destruction" do
-    before { FactoryGirl.create(:micropost, user: user) }
-
-    describe "as correct user" do
+    describe "micropost creation" do
       before { visit root_path }
-
-      it "should delete a micropost" do
-        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+  
+      describe "with invalid information" do
+  
+        it "should not create a micropost" do
+          expect { click_button "Submit" }.to_not change(Micropost, :count)
+        end
+  
+        describe "error messages" do
+          let(:error) { '1 error prohibited this micropost from being saved' }
+          before { click_button "Submit" }
+          it { should have_content(error) } 
+        end
+      end
+  
+      describe "with valid information" do
+  
+        before { fill_in 'micropost_content', with: "Lorem ipsum" }
+        it "should create a micropost" do
+          expect { click_button "Submit" }.to change(Micropost, :count).by(1)
+        end
+      end
+    end
+    
+    describe "micropost destruction" do
+      before { FactoryGirl.create(:micropost, user: user) }
+  
+      describe "as correct user" do
+        before { visit root_path }
+  
+        it "should delete a micropost" do
+          expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+        end
       end
     end
   end
